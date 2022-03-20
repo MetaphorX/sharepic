e8f84bb5d6a2c2b1b973dbad252a1de58dd00a84
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-03-20 10:19:24 +0000

    Add files via upload

--- sharepic_frontend/src/App.js
+++ sharepic_frontend/src/App.js
@@ -2,15 +2,13 @@ import React from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
-import { Search } from './components';
-
 
 const App = () => {
   return (
     <Routes>
       <Route path="login" element={<Login/>}/>
       <Route path="/" element={<Home/>}/>
-      <Route path="search" element={<Search/>}/>
+      
 
     </Routes>
   )
