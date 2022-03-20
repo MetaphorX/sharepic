ccf609527ff420538497f524d06c6f1c68fd4cbc
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-03-20 10:45:26 +0000

    Search not working well
    Once the search is clicked it loads a new page instead of allowin typing

--- sharepic_frontend/src/components/Navbar.jsx
+++ sharepic_frontend/src/components/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = ({searchTerm, setSearchTerm, user} ) => {
 
   const navigate = useNavigate();
 
-
+  if(!user) return null;
 
   return (
-    <div className="flex gap-2 md:gap-5 w-full pb-7">
+    <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
       <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
         <IoMdSearch fontSize={21} className="ml-1"/>
         <input 
           type="text"
-          onchange={(e) => setSearchTerm(e.target.value)}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search"
           value={searchTerm}
           onFocus={() => navigate('/search')}
