cd32d98e51cb64a698301f09696e7148bc03a43f
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-03-23 01:12:23 +0000

    Not being rendered...dunno why

---
+++ sharepic_frontend/src/components/CreatePin.jsx
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai'
+import { MdDelete } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
+import { client } from '../client'
+import Spinner from './Spinner'
+import {categories} from '../utils/data';
+
+const CreatePin = ({user}) => {
+  
+  const [title, setTitle] = useState('');
+  const [about, setabout] = useState('');
+  const [destination, setdestination] = useState('');
+  const [loading, setloading] = useState(false);
+  const [fields, setfields] = useState(null);
+  const [category, setcategory] = useState(null);
+  const [imageAsset, setimage] = useState(null);
+  const [wrongImageType, setwrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+  const uploadImage = (e)=>{
+    const {type} = e.target.files[0];
+    if (type === 'image/png' || type === 'image/svg' || type === 'image/jpeg' || type ==='image/gif' || type === 'image/tiff'){
+      setwrongImageType(false);
+      setloading();
+    }else{
+      setwrongImageType(true);
+    }
+
+  }
+
+    return (
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+        {fields &&(
+          <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">Please Fill in all the fields</p>
+        )}
+        <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+          <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+            <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+              {loading && <Spinner />}
+              {wrongImageType && <p>Wrong Image type</p>}
+              {!imageAsset ? (
+                <label>
+                  <div className="flex flex-col items-center justify-center h-full">
+                    <div className="flex flex-col justify-center items-center">
+                      <p className="font-bold text-2xl">
+                        <AiOutlineCloudUpload />
+                      </p>
+                      <p className="text-lg">
+                        Click to Upload
+                      </p>
+                    </div>
+                    <p className="mt-32 text-gray-400">
+                      Recommendation: Use high quality images
+                    </p>
+                  </div>
+                  <input 
+                    type="file"
+                    name="upload-image"
+                    onChange={uploadImage}
+                    className="w-0 h-0"
+                  />
+                </label>
+              ): (
+                <p>something else</p>
+              )}
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default CreatePin
