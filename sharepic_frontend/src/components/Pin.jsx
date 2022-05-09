77766fb45da37406eeef8644b8620e869e97b3a2
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-05-09 01:11:26 +0100

    Add files via upload

--- sharepic_frontend/src/components/Pin.jsx
+++ sharepic_frontend/src/components/Pin.jsx
@@ -13,7 +13,7 @@ const Pin = ({pin:{postedBy, image, _id, destination, save}}) => {
     const navigate = useNavigate();
     const user = fetchUser();
   
-    const alreadySaved = !!(save?.filter((item) =>item.postedBy._id === user.googleId))?.length;
+    const alreadySaved = !!(save?.filter((item) =>item?.postedBy?._id === user?.googleId))?.length;
      
     const savePin = (id) =>{
         if(!alreadySaved){
@@ -23,10 +23,10 @@ const Pin = ({pin:{postedBy, image, _id, destination, save}}) => {
             .setIfMissing({save: [] })
             .insert('after', 'save[-1]', [{
                 _key: uuidv4(),
-                userId: user.googleId,
+                userId: user?.googleId,
                 postedBy:{
                     _type: 'postedBy',
-                    _ref: user.googleId
+                    _ref: user?.googleId
                 }
 
             }])
@@ -97,7 +97,7 @@ const Pin = ({pin:{postedBy, image, _id, destination, save}}) => {
                                 {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
                             </a>
                         )}
-                        {postedBy?._id === user.googleId &&(
+                        {postedBy?._id === user?.googleId &&(
                             <button
                                 type="button"
                                 onClick ={(e) => {
